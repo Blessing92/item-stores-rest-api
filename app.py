0bb394899666a9473e40866e9a69decd20e39cf8
munga <percymunga02@gmail.com>
2021-07-28 16:18:43 +0300

    Changed app folder

--- app.py
+++ app.py
@@ -13,11 +13,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-uri = os.getenv("DATABASE_URL", 'sqlite:///data.db')
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
-app.config['SQLACHEMY_DATABASE_URI'] = uri
+app.config['SQLACHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLACHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'munga'
 api = Api(app)
